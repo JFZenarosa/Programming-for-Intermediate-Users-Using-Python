6fe0c51d2ec4229072889540af7ec42a98ccbb95
JFZenarosa <jaireh62@gmail.com>
2022-05-26 13:11:48 +0800

    Commit for Coding Exercise 3.8 read, readline, and for loop files

---
+++ Coding Exercise 3.8 for loop.py
@@ -0,0 +1,7 @@
+#Coding Exercise 3.8 for loop 
+f = open("pythonEssay.txt", "r")
+
+for read in f:
+    print(read)
+    
+f.close()
---
+++ Coding Exercise 3.8 read.py
@@ -0,0 +1,4 @@
+#Coding Exercise 3.8 read 
+f = open("pythonEssay.txt", "r")
+print(f.read())
+f.close()
