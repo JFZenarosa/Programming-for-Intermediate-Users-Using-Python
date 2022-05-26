ad1c92a8976aeea46356f3f77dee1065795d539f
JFZenarosa <jaireh62@gmail.com>
2022-05-26 14:10:14 +0800

    Commit for Coding Exercise 4.4 main and functionfile.py

---
+++ Coding Exercise 4.4 main.py
@@ -0,0 +1,7 @@
+#Coding Exercise 4.4 Main
+import functionfile as m 
+
+m.greeting("Jaireh")
+finddif = m.subnum(20,12)
+
+print("difference: ", finddif)
---
+++ functionfile.py
@@ -0,0 +1,16 @@
+# 4.4 Coding Exercises functions
+
+def greeting(name):
+    print("Are you ready to play some games, " + name + "?")
+
+def addnum(num1,num2):
+    return num1 + num2
+        
+def subnum(num1,num2):
+    return num1 - num2
+
+def dividenum(num1,num2):
+    return num1 / num2
+
+def multiplynum(num1,num2):
+    return num1 * num2
